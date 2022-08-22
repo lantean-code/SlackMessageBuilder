9bbd90325aa1b8c22dcb8e8c46a5fa51ae1cf850
Alex Jephson <alex.jephson@slimmingworld.co.uk>
2022-08-22 10:40:43 +0100

    Allow creation of custom message types derived from SlackMessageBase

--- SlackMessageBuilder/SlackMessageBuilder.cs
+++ SlackMessageBuilder/SlackMessageBuilder.cs
@@ -32,5 +32,16 @@ namespace Slack.MessageBuilder
         {
             return new MessageBuilder<SlackMessage>(text, isMarkdown);
         }
+
+        /// <summary>
+        /// Creates a new instance of the fluent message builder.
+        /// </summary>
+        /// <param name="text">The usage of this field changes depending on whether you're using blocks or not. If you are, this is used as a fallback string to display in notifications. If you aren't, this is the main body text of the message. It can be formatted as plain text, or with mrkdwn. This field is not enforced as required when using blocks, however it is highly recommended that you include it as the aforementioned fallback.</param>
+        /// <param name="isMarkdown">Determines whether the text field is rendered according to mrkdwn formatting or not. Defaults to true.</param>
+        /// <returns></returns>
+        public static MessageBuilder<T> CreateMessage<T>(string text, bool? isMarkdown = null) where T : SlackMessageBase
+        {
+            return new MessageBuilder<T>(text, isMarkdown);
+        }
     }
 }
